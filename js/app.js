265685e865dbc3865c1e0bc2240becdfbb1f3c29
raduanastase <raduanastasedev@gmail.com>
2016-03-18 17:36:51 +0200

    Second commit with rename to second git origin

--- js/app.js
+++ js/app.js
@@ -5,30 +5,49 @@ $(function () {
     var CALL_APPROVED = 'approved';
     var CALL_REJECTED = 'rejected';
     var $errorModal = $('#error-modal');
+    var $viewCaseTemplate = $('.view-case-template');
     var $addCaseForm = $('#add-case-form');
 
 
     function init() {
         getCases(CALL_ACCEPTED);
+        //getCase('1');
     }
 
     function getCases(typeOfCases) {
         $.ajax({
             type:'GET',
-            url: 'adra/get-cases',
+            url: '37.139.17.52/data.php?' + typeOfCases,
             dataType: 'json',
-            data: typeOfCases,
             success: onGetCasesSuccess,
             error: onGetCasesError
         })
     }
 
     function onGetCasesSuccess(data) {
-
+        $viewCaseTemplate
     }
 
     function onGetCasesError(data) {
         $errorModal.foundation('open');
+    }
+
+    function getCase(id) {
+        $.ajax({
+            type:'GET',
+            url: 'data.php?case_id='+id,
+            dataType: 'json',
+            success: onGetCaseSuccess,
+            error: onGetCaseError
+        })
+    }
+
+    function onGetCaseSuccess(data) {
+
+    }
+
+    function onGetCaseError(data) {
+
     }
 
     init();
