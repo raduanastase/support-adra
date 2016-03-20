c11580edaf2673aeb9a47cf7fbb0c5bcdc5c6283
gabrielharja <gabytza06@gmail.com>
2016-03-20 10:46:30 +0200

    cases count

--- data.php
+++ data.php
@@ -9,16 +9,17 @@
 
 
 		$result = mysqli_query($conn, "SELECT * FROM adra_cases WHERE ID = ".mysqli_real_escape_string($conn, $_GET['case_id']));
-		$result_image = mysqli_query($conn, "SELECT * FROM adra_cases_doc WHERE ID_case = ".mysqli_real_escape_string($conn, $_GET['case_id']));
+		$result_image = mysqli_query($conn, "SELECT file_path, private_type, is_cover_image FROM adra_cases_doc WHERE ID_case = ".mysqli_real_escape_string($conn, $_GET['case_id']));
 		$raspuns = array();
+		$raspuns_image = array();
 
 			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 				$raspuns[] = $row;
 			}
 			while ($row = mysqli_fetch_array($result_image, MYSQLI_ASSOC)) {
-				$raspuns[] = $row;
+				$raspuns_image[] = $row;
 			}
-		echo json_encode($raspuns);
+		echo json_encode(array($raspuns,$raspuns_image));
 		die();
 	}
 
@@ -27,12 +28,15 @@
 	{	$page=mysqli_real_escape_string($conn, $_GET["approved"]);
 		$page=($page-1)*10;
 		$result = mysqli_query($conn, "SELECT ac.ID, ac.case_name, ac.person_description, acd.file_path FROM adra_cases ac LEFT JOIN adra_cases_doc as acd on ac.ID = acd.ID_case WHERE ac.type=1 AND acd.is_cover_image = 1 ORDER BY ac.ID DESC LIMIT 10 OFFSET ".$page);
+
+		$case_count = mysqli_query($conn, "SELECT COUNT(*) AS case_count FROM adra_cases WHERE type= 1");
+
 		$raspuns = array();
 
 		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 			$raspuns[] = $row;
 		}
-		echo json_encode(array('success'=>true,$raspuns));
+		echo json_encode(array($case_count,$raspuns));
 		die();
 	}
 
