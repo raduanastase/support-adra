7a902b0d51f6062b4915a7f998405a4feb2bf726
gabrielharja <gabytza06@gmail.com>
2016-03-20 09:45:39 +0200

    minor changes

--- data.php
+++ data.php
@@ -4,20 +4,6 @@
    	session_start();
 
 
-	// if(isset($_GET["case_id"]) && isset($_SESSION['user'])) 
-	// {	
-
-
-	// 	$result = mysqli_query($conn, "SELECT * FROM adra_cases WHERE ID = ".mysqli_real_escape_string($conn, $_GET['case_id']));
-	// 	$raspuns = array();
-
-	// 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-	// 		$raspuns[] = $row;
-	// 	}
-	// 	echo json_encode(array('success'=>true,$raspuns));
-	// 	die();
-	// }
-
 	if(isset($_GET["case_id"])) 
 	{	
 
