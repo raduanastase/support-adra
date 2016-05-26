4f6c105852227e85a31cb17ad058c1bff00021a7
raduanastase <raduanastasedev@gmail.com>
2016-05-26 06:48:02 +0300

    Created Laravel API for Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,15 +8,48 @@ use App\Http\Requests;
 
 class PostsController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        $cases = Post::all();
-
+        $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
 
-    public function show($post)
+    public function create()
     {
-        return $post;
+        $post = new Post;
+        return view('posts.edit', compact('post'));
+    }
+
+    public function store()
+    {
+        $post = new Post;
+        $post->fill(/*$request->only($post->getFillable())*/);
+        $post->save();
+        return redirect()->route('post.index')->with('success_message', 'The post has been successfully saved.');
+    }
+
+    public function show($id)
+    {
+        return Post::find($id);
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update($id)
+    {
+        $post = Post::find($id);
+        $post->fill(/*$request->only($post->getFillable())*/);
+        $post->save();
+        return redirect()->route('post.index')->with('success_message', 'The post has been successfully saved.');
+    }
+
+    public function destroy($id)
+    {
+        Post::find($id)->delete();
+        return redirect()->route('post.index')->with('success_message', 'The post has been successfully deleted.');
     }
 }
