84127ea4a44370885200de6f8ea9aedd4f49f71c
raduanastase <raduanastasedev@gmail.com>
2016-07-27 08:07:16 +0300

    Added ES6 to editor - not sure if es6 syntax works with gulp-elixir

---
+++ resources/assets/js/mine/views/AppView/TabView/PostsCollection.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//var $ = global.$ = global.jQuery = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+module.exports = Backbone.Collection.extend({
+    url: function () {
+        return 'posts/type/'+this.type;
+    },
+    
+    initialize: function (type) {
+        this.type = type;
+    }
+});
