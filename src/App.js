b54ffa7b840c3657a961e49fedef61f3de375f1a
raduanastase <raduanastasedev@gmail.com>
2018-01-16 21:24:21 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 // @flow
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-class App extends Component<{}, {}> {
+const App = () => (
+  <Router>
+    <div>
+      <Header/>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+    </div>
+  </Router>
+);
+
+const Header = () => (
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+  </div>
+);
+
+class Home extends PureComponent<{}, {}> {
   render() {
     return (
       <div className={styles.App}>
@@ -14,6 +37,16 @@ class App extends Component<{}, {}> {
         <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
+    );
+  }
+}
+
+class About extends PureComponent<{}, {}> {
+  render() {
+    return (
+      <div className={styles.App}>
+        ABOUT
       </div>
     );
   }
